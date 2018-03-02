151677afee1a595892e30fcd3807d691de15d5f8
Eduardo Enriquez <eenriquez@onapsis.com>
2018-03-02 13:22:54 -0300

    added readme and openexchange

--- api_dolar.py
+++ api_dolar.py
@@ -5,22 +5,21 @@ from keys import AUTH, BASE_URL
 
 logger = logging.getLogger(__name__)
 
-dolar_api = 'http://ws.geeklab.com.ar/dolar/get-dolar-json.php'
+dolar_api = 'https://openexchangerates.org/api/latest.json'
 
 
 def get_dolar():
-    r = requests.get(dolar_api)
+    r = requests.get(dolar_api, params={'app_id': '83f44a6566f945a4abc92139fd0cc60d'})
 
     if r.status_code != 200:
         text = "Perdón! La api no está  disponible!"
         return text
 
     data = r.json()
-
     if not data:
         text = "Perdón! La api no está  disponible!"
         return text
 
-    text = f'USD oficial {data["libre"]} - Blue {data["blue"]}'
+    text = f'USD oficial {data['rates']['ARS']}'
 
     return text
