4f076fa3598b902f0ddcbbe2043d245c0fe82170
Eduardo Enriqez (eduzen) <eduardo.a.enriquez@gmail.com>
2018-04-03 00:28:47 -0300

    added help command

--- src/handlers/commands/questions.py
+++ src/handlers/commands/questions.py
@@ -12,6 +12,7 @@ def q_menu(bot, update, args):
     keyboard = [
         InlineKeyboardButton("Only questions", callback_data="questions"),
         InlineKeyboardButton("Questions & answers", callback_data="answer"),
+        InlineKeyboardButton("How to use it", callback_data="help"),
     ]
     reply_markup = InlineKeyboardMarkup(build_menu(keyboard, n_cols=2))
 
@@ -40,9 +41,17 @@ def button(bot, update):
         answer = get_questions()
     elif selected == "answer":
         answer = get_questions(answer=True)
+    elif selected == "help":
+        answer = (
+            "Primero debes agregar una pregunta usando `/add_question <tu pregunta>`\n"
+            "Esto te va a responder con un id, por ejemplo 1\n"
+            "Para agregar la respuesta a tu pregunta usamos ese id: `/add_answer 1 <tu respuesta>`\n"
+            "Despues resta hablarle al bot `tu pregunta? @eduzen_bot`"
+        )
 
     bot.edit_message_text(
         text=f"{answer}",
         chat_id=query.message.chat_id,
         message_id=query.message.message_id,
+        parse_mode="Markdown",
     )
