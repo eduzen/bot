3e3283294682f13c0fe6b02429b9a760b7caa436
Eduardo Enriqez (eduzen) <eduardo.a.enriquez@gmail.com>
2018-05-21 00:09:24 -0300

    changed inline

--- eduzen_bot/plugins/messages/inline.py
+++ eduzen_bot/plugins/messages/inline.py
@@ -1,7 +1,8 @@
 import logging
 from uuid import uuid4
 
-from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode
+from telegram.utils.helpers import escape_markdown
 
 logger = logging.getLogger()
 
@@ -12,13 +13,30 @@ def code_markdown(bot, update):
     if not query:
         return
 
-    query = f"```\n{query} \n```"
-
     results = [
         InlineQueryResultArticle(
             id=uuid4().hex[:8],
             title="code",
-            input_message_content=InputTextMessageContent(query, "MARKDOWN"),
-        )
+            input_message_content=InputTextMessageContent(
+                f"```\n{query}\n```",
+                parse_mode=ParseMode.MARKDOWN),
+        ),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Caps",
+            input_message_content=InputTextMessageContent(
+                query.upper())),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Bold",
+            input_message_content=InputTextMessageContent(
+                "*{}*".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN)),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Italic",
+            input_message_content=InputTextMessageContent(
+                "_{}_".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN))
     ]
     bot.answer_inline_query(update.inline_query.id, results)
