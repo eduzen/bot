d1e27e1a0b6dd01adc962cc881eb1d3764dd55b7
Eduardo Enriquez <eduardo.enriquez@bcc.gmbh>
2019-04-22 13:20:00 +0200

    added new changes

--- eduzen_bot/plugins/messages/message.py
+++ eduzen_bot/plugins/messages/message.py
@@ -183,41 +183,30 @@ def prepare_text(text):
 
 @run_async
 def parse_msgs(bot, update):
-
-    username = update.message.from_user.username
-    chat_id = update.message.chat_id
-
-    logger.info("parse_msgs... by %s", username)
-
-    # record_msg(update.message.from_user.name, update.message.text, chat_id=chat_id)
-
-    # get_or_create_user(update.message.from_user)
-
-    text = prepare_text(update.message.text)
-    blob = TextBlob(text)
-
     entities = update.message.parse_entities()
-    if not entities:
-        answer, gif = parse_regular_chat(blob)
-        if answer:
-            bot.send_chat_action(
-                chat_id=update.message.chat_id, action=ChatAction.TYPING
-            )
-        if answer and not gif:
-            bot.send_message(chat_id=update.message.chat_id, text=answer)
-        elif answer and gif:
-            bot.send_document(chat_id=update.message.chat_id, document=answer)
-        return
-
     mentions = [
         value
         for key, value in entities.items()
         if "@eduzen_bot" in value or "@eduzenbot" in value
     ]
+
     if not mentions:
         return
 
-    bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-    answer = parse_chat(blob)
+    username = update.message.from_user.username
+    logger.info("parse_msgs... by %s", username)
 
-    bot.send_message(chat_id=update.message.chat_id, text=answer)
+    text = prepare_text(update.message.text)
+    blob = TextBlob(text)
+
+    answer, gif = parse_regular_chat(blob)
+    if answer:
+        bot.send_chat_action(
+            chat_id=update.message.chat_id, action=ChatAction.TYPING
+        )
+    if answer and not gif:
+        bot.send_message(chat_id=update.message.chat_id, text=answer)
+    elif answer and gif:
+        bot.send_document(chat_id=update.message.chat_id, document=answer)
+
+    return
