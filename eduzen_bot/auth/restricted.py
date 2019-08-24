252999e0700ed208904bbda645071dfc5c84e9e5
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2019-08-24 18:16:24 +0200

    Better logging

--- eduzen_bot/auth/restricted.py
+++ eduzen_bot/auth/restricted.py
@@ -9,9 +9,9 @@ logger = structlog.get_logger(filename=__name__)
 def restricted(func):
     @wraps(func)
     def wrapped(bot, update, *args, **kwargs):
-        user_id = update.effective_user.id
-        if user_id not in LIST_OF_ADMINS:
-            logger.error(f"{func.__name__} - Unauthorized access denied for {user_id} -  {user}.")
+        user = update.effective_user
+        if user.id not in LIST_OF_ADMINS:
+            logger.error(f"{func.__name__} - Unauthorized access denied for {user}.")
             return
 
         return func(bot, update, *args, **kwargs)
