64c6de7ebe9d94ba799277f3a463f62e452796cc
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2022-02-23 00:42:25 +0100

    :bug: Add database url default value

--- eduzen_bot/models.py
+++ eduzen_bot/models.py
@@ -16,7 +16,7 @@ from peewee import (
 from playhouse.db_url import connect
 from playhouse.shortcuts import ThreadSafeDatabaseMetadata, model_to_dict
 
-database_url = os.getenv("DATABASE_URL")
+database_url = os.getenv("DATABASE_URL", "sqlite:///default.db")
 db = connect(database_url)
 
 
